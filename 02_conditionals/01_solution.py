2bd2058a7f0dfa1d2dd9dbd6649d8b7e788a2738
pratham-vbhat <pratham.bhat@asmltd.com>
2025-10-27 14:48:00 +0530

    added 01_solution.py file as a solution for age group cateorization

--- 02_conditionals/01_solution.py
+++ 02_conditionals/01_solution.py
@@ -8,9 +8,12 @@ def check_age(age):
     elif age > 60:
         return "Senior Citizen"
 
+
 user_name = input("Enter the person's name: ")
 user_age = int(input(f"Enter the {user_name}'s age: "))
 
 age_check = check_age(user_age)
 
-print(f"According to the age of {user_name} his/her age is {user_age} and he/she is an {age_check}")
+print(
+    f"According to the age of {user_name} his/her age is {user_age} and he/she is an {age_check}"
+)
