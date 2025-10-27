f793a63eb257f1692c13e7570d1cfa26e2acfd87
pratham-vbhat <pratham.bhat@asmltd.com>
2025-10-27 12:37:54 +0530

    completed dictonaries

--- 01_basics/dictonatory.py
+++ 01_basics/dictonatory.py
@@ -23,3 +23,34 @@ print(chai_types)
 
 for key, values in chai_types.items():
     print(f"{key} is very {values}")
+
+chai_types.pop("Ginger")
+
+chai_types.popitem()
+
+del chai_types["Green"]
+chai_types_copy = chai_types.copy()
+
+tea_shops = {
+    "chai": {
+        "Masala" : "Spicy",
+        "Ginger" : "Zesty",
+    },
+    "Tea" : {
+        "Green" : "Fresh",
+        "Black" : "Strong"
+    },
+}
+
+print(tea_shops.get("chai"))
+
+squared_number = {x : x**2 for x in range(6)}
+
+# squared_number.clear() # Use this when you want to clear a dictionary.
+
+keys = ["Masala", "Ginger", "Lemon"]
+
+default_value = "Delious"
+new_dict = dict.fromkeys(keys, default_value)
+
+# new_dict = dict.fromkeys(keys, keys)
