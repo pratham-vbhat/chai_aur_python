74073e034742faa21287614bb896170922582f5e
pratham-vbhat <pratham.bhat@asmltd.com>
2025-10-28 11:15:33 +0530

    added solution for Weather Activity Suggestion Question

---
+++ 02_conditionals/05_solution.py
@@ -0,0 +1,20 @@
+'''
+Question-5: Weather Activity Suggestion
+'''
+
+def weather_activity(weather):
+    if weather.lower() == "sunny":
+        return "go for a walk"
+    elif weather.lower() == "rainy":
+        return "read a Book"
+    elif weather.lower() == "winter":
+        return "build a Snowman"
+    else:
+        return f"Invalid Weather or currently we do not have any information on the {weather}"
+
+user_name = input("Kindly enter your name: ")
+weather = input(f"{user_name}, could you kindly enter the current weather: ")
+
+result = weather_activity(weather)
+
+print(f"Hy {user_name}, the weather outside is {weather}, so you could {result}!!!.")
