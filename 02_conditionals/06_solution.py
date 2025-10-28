c596dab96973a7486f3a0d71ba5ddfd10298d907
pratham-vbhat <pratham.bhat@asmltd.com>
2025-10-28 11:34:45 +0530

    added solution for Transport Mode Selection

---
+++ 02_conditionals/06_solution.py
@@ -0,0 +1,18 @@
+'''
+Question-6: Transport Mode Seletction:
+'''
+
+def select_transport_mode(distance):
+    if distance < 3:
+        return "walk"
+    elif 3<= distance < 15:
+        return "take bike"
+    elif distance >= 15:
+        return "take car"   
+
+user_name = input("Enter your name: ")
+distance = int(input("Kindly enter the distance you want to travel: "))
+
+mode = select_transport_mode(distance)
+
+print(f"Hy {user_name}. So to travel {distance} Km, you could {mode}")
