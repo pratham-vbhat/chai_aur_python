bd98fc0d682ad3af4a2491d36ee135c7bc0ce150
pratham-vbhat <pratham.bhat@asmltd.com>
2025-11-05 15:31:31 +0530

    added solution of leap year checker

---
+++ 02_conditionals/09_solution.py
@@ -0,0 +1,19 @@
+'''
+Question-9. Leap Year Checker
+'''
+
+def check_leap_year(year):
+    if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+        return True
+    else:
+        return False
+
+
+year = int(input("Enter the year: "))
+
+leap_year = check_leap_year(year)
+
+if leap_year:
+    print(f"Hy there, {year} year is a leap year!")
+else:
+    print(f"Hy there, {year} year is a not leap year!")
