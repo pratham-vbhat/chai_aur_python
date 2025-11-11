e75e1e5fc0e7e6bb6ba8c8e39f291133a795b41a
pratham-vbhat <pratham.bhat@asmltd.com>
2025-11-11 13:15:37 +0530

    loops: added method-2 solution of question-4

--- 03_loops/04_solution.py
+++ 03_loops/04_solution.py
@@ -1,9 +1,20 @@
 # Question-4: Reverse a String using a Loop
 
 text: str = input("Enter a string: ")
-rev_text: str = ''
+rev_text: str = ""
+
+# Method-1
 
 for char in text:
-    rev_text  = char + rev_text
+    rev_text = char + rev_text
 
-print(f"The reverse form of this \"{text}\" text is: \"{rev_text}\"")
+print("Method-1")
+print(f'The reverse form of this "{text}" text is: "{rev_text}"')
+
+# Method-2:
+
+for ch in text:
+    rev_text += ch[:-1]
+
+print("Method-2")
+print(f'The reverse form of this "{text}" text is: "{rev_text}"')
